41ac694388057688d2389934b58799594675e6e7
Mauricio Macedo da Silva <macedo.mauriciogen@gmail.com>
2025-11-07 12:43:18 -0300

    feat: estrutura frontend criada

--- api/routes/web.php
+++ api/routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response("Api V1 running", 200);
 });
---
+++ app/src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <h1>Olá Word</h1>
+</template>
+
+<style scoped>
+h1 {
+  color: #cecece;
+}
+</style>
