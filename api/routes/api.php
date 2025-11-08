d58b3aea7fbab8d5ccaf1976d20f009d8073033c
Mauricio Macedo da Silva <macedo.mauriciogen@gmail.com>
2025-11-08 11:53:15 -0300

    fix: correção de erros na api para token

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,10 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')
-    ->middleware(['api'])
-    ->group(function () {
-        require base_path('routes/v1/auth_routes.php');
-        require base_path('routes/v1/candidate/candidate_routes.php');
-    });
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
--- api/routes/web.php
+++ api/routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response("Api V1 running", 200);
+    return view('welcome');
 });
