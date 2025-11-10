053e88275f584ebcf03b4a16c88bab8f7148e0b9
Mauricio Macedo da Silva <macedo.mauriciogen@gmail.com>
2025-11-10 12:45:49 -0300

    feat: remoção de arquivos desnecessarios

--- app/src/services/index.js
+++
@@ -1,5 +0,0 @@
-import { createGenericService } from "./genericService";
-
-export const moviesService = createGenericService("movies");
-export const rentalsService = createGenericService("rentals");
-export const usersService = createGenericService("users");
